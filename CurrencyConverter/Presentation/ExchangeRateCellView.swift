c32f6349b9105678cc11144dd64d8682bf6e65ce
minneee <alsgml022100@naver.com>
2025-09-26 19:26:44 +0900

    [feat] 테이블 뷰 추가

---
+++ CurrencyConverter/Presentation/ExchangeRateCellView.swift
@@ -0,0 +1,74 @@
+//
+//  ExchangeRateCellView.swift
+//  CurrencyConverter
+//
+//  Created by 김민희 on 9/26/25.
+//
+
+import UIKit
+import SnapKit
+
+final class ExchangeRateCellView: UITableViewCell {
+  static let id = "ExchangeRateCellView"
+
+  let currencyNameLabel: UILabel = {
+    let label = UILabel()
+    label.text = "KRW"
+    label.textColor = .text
+    label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+    return label
+  }()
+
+  let exchangeRateLabel: UILabel = {
+    let label = UILabel()
+    label.text = "0"
+    label.textColor = .text
+    label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+    return label
+  }()
+
+  let spacerView: UIView = {
+    let view = UIView()
+    view.setContentHuggingPriority(.defaultLow, for: .horizontal)
+    return view
+  }()
+
+  let exchangeRateStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .horizontal
+    return stackView
+  }()
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    configureUI()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func configureUI() {
+    contentView.backgroundColor = .background
+    [
+      exchangeRateStackView
+    ].forEach { contentView.addSubview($0) }
+
+    [
+      currencyNameLabel,
+      spacerView,
+      exchangeRateLabel
+    ].forEach { exchangeRateStackView.addArrangedSubview($0) }
+
+    exchangeRateStackView.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(30)
+      $0.centerY.equalToSuperview().inset(15)
+    }
+  }
+
+  private func configureCell() {
+    currencyNameLabel.text = "currencyName"
+    exchangeRateLabel.text = "exchangeRate"
+  }
+}
+
